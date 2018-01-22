d7b570e3a41c3df90529c4d641b37d1c9c3454d1
Tess Craig <tessashleycraig89@gmail.com>
2018-01-22 10:36:37 -0600

    generateFavoriteHTML works

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   $.get('/api/favorites').success(function (favorites) {
     console.log(favorites);
     favorites.forEach(function(favorite) {
-      render(favorite);
+      renderFavorite(favorite);
       console.log('hi');
     });
 
@@ -54,38 +54,38 @@ $(document).ready(function() {
   // }
 
 // generate just the html for an Favorite row
-//   function generateFavoriteHtml(favorite) {
-//     var favoriteHtml =
-//     "        <!-- one favorite -->" +
-//                 // each favorite has data-favorite-id attribute that's value is favorite._id
-//       "        <div class='row favorite' data-favorite-id='" + favorite._id + "'>" +
-//       "          <div class='col-md-10 col-md-offset-1'>" +
-//       "            <div class='panel panel-default'>" +
-//       "              <div class='panel-body'>" +
-//       "              <!-- begin favorite internal row -->" +
-//       "                <div class='row'>" +
-//       "                  <div class='col-md-3 col-xs-12 thumbnail favorite-art'>" +
-//       "                     <img src='" + "http://placehold.it/400x400'" +  " alt='favorite image'>" +
-//       "                  </div>" +
-//       "                  <div class='col-md-9 col-xs-12'>" +
-//       "                    <ul class='list-group'>" +
-//       "                      <li class='list-group-item'>" +
-//       "                        <h4 class='inline-header'>Favorite Title:</h4>" +
-//       "                        <span class='favorite-title'>" + favorite.title + "</span>" +
-//       "                      </li>" +
-//       "                      <li class='list-group-item'>" +
-//       "                        <h4 class='inline-header'>Favorite Address:</h4>" +
-//       "                        <span class='address'>" + favorite.address + "</span>" +
-//       "                      </li>" +
-//       "<!-- end one album -->";
-//         return favoriteHtml;
-// }
-//
+  function generateFavoriteHtml(favorite) {
+    var favoriteHtml =
+    "        <!-- one favorite -->" +
+                // each favorite has data-favorite-id attribute that's value is favorite._id
+      "        <div class='row favorite' data-favorite-id='" + favorite._id + "'>" +
+      "          <div class='col-md-10 col-md-offset-1'>" +
+      "            <div class='panel panel-default'>" +
+      "              <div class='panel-body'>" +
+      "              <!-- begin favorite internal row -->" +
+      "                <div class='row'>" +
+      "                  <div class='col-md-3 col-xs-12 thumbnail favorite-art'>" +
+      "                     <img src='" + "http://placehold.it/400x400'" +  " alt='favorite image'>" +
+      "                  </div>" +
+      "                  <div class='col-md-9 col-xs-12'>" +
+      "                    <ul class='list-group'>" +
+      "                      <li class='list-group-item'>" +
+      "                        <h4 class='inline-header'>Favorite Title:</h4>" +
+      "                        <span class='favorite-title'>" + favorite.title + "</span>" +
+      "                      </li>" +
+      "                      <li class='list-group-item'>" +
+      "                        <h4 class='inline-header'>Favorite Address:</h4>" +
+      "                        <span class='address'>" + favorite.address + "</span>" +
+      "                      </li>" +
+      "<!-- end one album -->";
+        return favoriteHtml;
+}
+
 // // this function takes a single favorite and renders it to the page
 //   //edit the function renderFavorite to display one Favorite on the page.
-// function renderFavorite(favorite) {
-//     var html = generateFavoriteHtml(favorite);
-//     console.log('rendering favorite:', favorite);
-//
-//     $('#favorites').prepend(html);
-// }
+function renderFavorite(favorite) {
+    var html = generateFavoriteHtml(favorite);
+    console.log('rendering favorite:', favorite);
+
+    $('#favorites').prepend(html);
+}
