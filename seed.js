355a24d88b0adae4a947882f7905c68b0a5c3841
Tess Craig <tessashleycraig89@gmail.com>
2018-01-22 11:10:50 -0600

    puts data in seed.js

--- seed.js
+++ seed.js
@@ -4,19 +4,24 @@ var favoriteList = [];
 
 //push art objects into array
 favoriteList.push({
+    title: 'Band Together',
+    image: '/images/bangTogether.jpg',
+    address: 'E. 6th & Chicon'
+});
+favoriteList.push({
+    title: "You're My Butter Half",
+    image: '/images/butter.jpg',
+    address: '2000 E MLK Jr Blvd'
+});
+favoriteList.push({
+    title: 'Chicon Wall',
+    image: '/images/chicon.jpg',
+    address: 'E. 12th & Chicon'
+});
+favoriteList.push({
     title: 'Til Death',
-    image: 'public/images/tillDeath.jpg',
-    address: 'address1'
-});
-favoriteList.push({
-    title: 'title2',
-    image: 'image2',
-    address: 'address2'
-});
-favoriteList.push({
-    title: 'title3',
-    image: 'image3',
-    address: 'address3'
+    image: '/images/tillDeath.jpg',
+    address: 'E. 7th & Waller'
 });
 
 db.Favorite.remove({}, function(err, favorites){
